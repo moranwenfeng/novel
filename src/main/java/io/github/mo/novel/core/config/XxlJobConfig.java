6c48e6ca19ddfa76ff616b53f4f172ae17d1b3b8
墨丶文 <327743586@qq.com>
2022-06-09 16:31:46 +0800

    分布式任务调度平台调度XXL-JOB集成与配置（1）

---
+++ src/main/java/io/github/mo/novel/core/config/XxlJobConfig.java
@@ -0,0 +1,42 @@
+package io.github.mo.novel.core.config;
+
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * XXL-JOB 配置类
+ *
+ * @author mo
+ * @date 2022/6/9 15:20
+ */
+@Configuration
+@Slf4j
+public class XxlJobConfig {
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appName;
+
+    @Value("${xxl.job.executor.logpath")
+    private String logPath;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        log.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        return xxlJobSpringExecutor;
+    }
+}
