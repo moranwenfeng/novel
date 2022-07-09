f857964b8c2a69c12c1b7dc30cf65bf6a12d3e04
墨丶文 <327743586@qq.com>
2022-07-09 17:15:57 +0800

    前台门户-资源(图片/视频/文档)模块 API 控制器

--- src/main/java/io/github/mo/novel/controller/front/ResourceController.java
+++ src/main/java/io/github/mo/novel/controller/front/ResourceController.java
@@ -2,16 +2,16 @@ package io.github.mo.novel.controller.front;
 
 import io.github.mo.novel.core.common.constant.ApiRouterConsts;
 import io.github.mo.novel.core.common.resp.RestResp;
+import io.github.mo.novel.dto.resp.ImgVerifyCodeRespDto;
 import io.github.mo.novel.service.ResourceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * 前台门户-资源(图片/视频/文档)模块 API 控制器
@@ -28,6 +28,15 @@ public class ResourceController {
 
     /**
      * 获取图片验证码接口
+     */
+    @Operation(summary = "获取图片验证码接口")
+    @GetMapping("img_verify_code")
+    public RestResp<ImgVerifyCodeRespDto> getImgVerifyCode() throws IOException {
+        return resourceService.getImgVerifyCode();
+    }
+
+    /**
+     * 图片上传接口
      */
     @Operation(summary = "图片上传接口")
     @PostMapping("/image")
