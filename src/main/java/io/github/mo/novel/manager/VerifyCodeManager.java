a914c30bcd2a9da3c72bb05931d4c7662e1401a4
墨丶文 <327743586@qq.com>
2022-07-09 16:00:24 +0800

    验证码 管理类

---
+++ src/main/java/io/github/mo/novel/manager/VerifyCodeManager.java
@@ -0,0 +1,52 @@
+package io.github.mo.novel.manager;
+
+import io.github.mo.novel.core.common.constant.CacheConsts;
+import io.github.mo.novel.core.common.util.ImgVerifyCodeUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Objects;
+
+/**
+ * 验证码 管理类
+ *
+ * @author mo
+ * @date 2022/7/9 15:35
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class VerifyCodeManager {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 生成图形验证码，并放入Redis中
+     */
+    public String genImgVerifyCode(String sessionId) throws IOException {
+        String verifyCode = ImgVerifyCodeUtils.getRandomVerifyCode(4);
+        String img = ImgVerifyCodeUtils.genVerifyCodeImg(verifyCode);
+        stringRedisTemplate.opsForValue().set(CacheConsts.IMG_VERIFY_CODE_CACHE_KEY + sessionId, verifyCode, Duration.ofMinutes(5));
+        return img;
+    }
+
+    /**
+     * 校验图形验证码
+     */
+    public boolean imgVerifyCodeOk(String sessionId, String verifyCode) {
+        return Objects.equals(
+                stringRedisTemplate.opsForValue().get(CacheConsts.IMG_VERIFY_CODE_CACHE_KEY + sessionId),verifyCode
+        );
+    }
+
+    /**
+     * 从redis中删除验证码
+     */
+    public void removeImgVerifyCode(String sessionId) {
+        stringRedisTemplate.delete(CacheConsts.IMG_VERIFY_CODE_CACHE_KEY + sessionId);
+    }
+}
